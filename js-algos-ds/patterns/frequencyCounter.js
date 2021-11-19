128b45a3c7a7510d5e62886f65d952ec4999bee7
Karem Ceron <karemiceron@gmail.com>
2021-11-19 16:39:01 -0500

    naive approach

--- js-algos-ds/patterns/frequencyCounter.js
+++ js-algos-ds/patterns/frequencyCounter.js
@@ -6,6 +6,22 @@
 // same([1,2,3], [1,9])   // false
 // same([1,2,1], [4,4,1]) // false (must be same frequency)
 
+// naive approach
 const same = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
 
+  for (let i = 0; i < arr1.length; i++) {
+    let index = arr2.indexOf(arr1[i] ** 2)
+    if (index === -1) {
+      return false;
+    }
+    arr2.splice(index, 1)
+  }
+  return true;
 };
+
+console.log(same([1,2,3,2], [9,1,4,4]));
+console.log(same([1,2,3], [1,9]));
+console.log(same([1,2,1], [4,4,1]));
