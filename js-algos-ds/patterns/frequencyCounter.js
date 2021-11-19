bfaa595b2b36086b67a5e54811cc15089274922c
Karem Ceron <karemiceron@gmail.com>
2021-11-19 16:00:22 -0500

    first problem

---
+++ js-algos-ds/patterns/frequencyCounter.js
@@ -0,0 +1,11 @@
+// write a function called same, which accepts 2 arrays. 
+// It should return true if every value in the arr has it's
+// corresponding value squared in the second array. 
+// The frequeny of values must be the same.
+// same([1,2,3], [4,1,9]) // true
+// same([1,2,3], [1,9])   // false
+// same([1,2,1], [4,4,1]) // false (must be same frequency)
+
+const same = (arr1, arr2) => {
+
+};
