9a2e4336af65b97e76511da7b5c45e8cbb7598bb
Karem Ceron <karemiceron@gmail.com>
2021-11-19 17:12:31 -0500

    sameRefactored

--- js-algos-ds/patterns/frequencyCounter.js
+++ js-algos-ds/patterns/frequencyCounter.js
@@ -1,27 +1,55 @@
-// write a function called same, which accepts 2 arrays. 
+// write a function called same, which accepts 2 arrays.
 // It should return true if every value in the arr has it's
-// corresponding value squared in the second array. 
+// corresponding value squared in the second array.
 // The frequeny of values must be the same.
 // same([1,2,3], [4,1,9]) // true
 // same([1,2,3], [1,9])   // false
 // same([1,2,1], [4,4,1]) // false (must be same frequency)
 
-// naive approach
+// naive approach O(n^2)
 const same = (arr1, arr2) => {
   if (arr1.length !== arr2.length) {
     return false;
   }
 
   for (let i = 0; i < arr1.length; i++) {
-    let index = arr2.indexOf(arr1[i] ** 2)
+    let index = arr2.indexOf(arr1[i] ** 2);
     if (index === -1) {
       return false;
     }
-    arr2.splice(index, 1)
+    arr2.splice(index, 1);
   }
   return true;
 };
 
-console.log(same([1,2,3,2], [9,1,4,4]));
-console.log(same([1,2,3], [1,9]));
-console.log(same([1,2,1], [4,4,1]));
+// REFACTORED O(n) linear time
+const sameRefactored = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  let freqCounter1 = {};
+  let freqCounter2 = {};
+  for (let val of arr1) {
+    freqCounter1[val] = (freqCounter1[val] || 0) + 1;
+  }
+  for (let val of arr2) {
+    freqCounter2[val] = (freqCounter2[val] || 0) + 1;
+  }
+
+  for (let key in freqCounter1) {
+    if (!(key ** 2 in freqCounter2)) {
+      return false;
+    }
+    if (freqCounter2[key ** 2] !== freqCounter1[key]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+
+// console.log(sameRefactored([1,2,3,2], [9,1,4,4]));
+// console.log(sameRefactored([1,2,3], [1,9]));
+// console.log(sameRefactored([1,2,1], [4,4,1]));
