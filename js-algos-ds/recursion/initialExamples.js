480cebd1fa71ed44e2d33a8d96ee32020b2d4e5f
Karem Ceron <karemiceron@gmail.com>
2022-08-30 18:37:33 -0400

    helper recursion

--- js-algos-ds/recursion/initialExamples.js
+++ js-algos-ds/recursion/initialExamples.js
@@ -25,3 +25,24 @@ function factorialR() {
 
 console.log(factorial(3));
 console.log(factorial(4));
+
+
+// helper method 
+function collectOdds(nums) {
+  let result = [];
+
+  function helper(helperInput) {
+    if(helperInput.length === 0) {
+      return;
+    }
+    if(helperInput[0] % 2 !== 0) {
+      result.push(helperInput[0])
+    }
+
+    helper(helperInput.slice(1))
+  }
+
+  helper(arr)
+
+  return result
+}
